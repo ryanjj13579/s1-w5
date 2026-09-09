de156f3496bf63830ace6a7a2d7fa386e7014395
Ryan Jung <ryanjj13579@gmail.com>
2026-09-09 11:40:50 -0700

    Week 5 Day 1

--- WordGuessing.java
+++ WordGuessing.java
@@ -1,18 +1,23 @@
 public class WordGuessing {
-    private String wordToBeGuessed;
-    private String userString = "_____";// 5 underscores
-  
-    //we will say that secret must be five characters
-    public WordGuessing(String secret) {
-  
-    }
-  
-    public void update(String letter) {
-  
-    }
-  
-    public String getUserString(){
-      
-    }
+  private String wordToBeGuessed;
+  private String userString = "_____";// 5 underscores
+
+  // we will say that secret must be five characters
+  public WordGuessing(String secret) {
+    wordToBeGuessed = secret;
   }
-  
+
+  // pre-condition:
+  // - letter is in word
+  // - word is 5 characters
+  // substring(1param); substring (2-param)
+  // indexOf, length
+  public void update(String letter) {
+    int letPosition = wordToBeGuessed.indexOf(letter);
+    userString = userString.substring(0, letPosition) + letter + userString.substring(letPosition+1);
+  }
+
+  public String getUserString() {
+    return userString;
+  }
+}
--- WordGuessingMain.java
+++ WordGuessingMain.java
@@ -1,5 +1,15 @@
 public class WordGuessingMain{
     public static void main(String[] args){
-        
+        WordGuessing w = new WordGuessing("witch");
+        w.update("t");
+        System.out.println(w.getUserString());
+        w.update("i");
+        System.out.println(w.getUserString());
+        w.update("w");
+        System.out.println(w.getUserString());
+        w.update("c");
+        System.out.println(w.getUserString());
+        w.update("h");
+        System.out.println(w.getUserString());
     }
 }
